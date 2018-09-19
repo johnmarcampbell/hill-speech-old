22ab175c11b4c0b11a6ddc24180abaed2e61df78
John Campbell <johnmarcampbell@gmail.com>
2018-09-19 13:15:41 -0400

    Add basic bokeh plot for speaker topics

--- hillspeech/helpers.py
+++ hillspeech/helpers.py
@@ -1,4 +1,42 @@
 from pymongo import MongoClient
+from bokeh.models import ColumnDataSource
+from bokeh.plotting import figure
+
+# These are hard-coded topic labels for a particular model.
+# This is very brittle and will have to change later.
+TOPIC_LABELS = [
+    (1, 'Legislation'),
+    (3, 'Committees'),
+    (4, 'Community'),
+    (5, 'Finance'),
+    (6, 'Budget'),
+    (7, 'Laws'),
+    (9, 'Education'),
+    (10, 'Healthcare'),
+    (11, 'Military Service'),
+    (14, 'International Relations'),
+    (16, 'Economics'),
+    (18, 'Employment'),
+    (19, 'Procedure'),
+    (20, 'Defense'),
+    (22, 'History'),
+    (24, 'Environment'),
+    (26, 'Technology'),
+    (28, 'Disaster'),
+    (29, 'Resolutions/Bills'),
+    (31, 'Energy'),
+    (32, 'Taxes'),
+    (33, 'Healthcare II'),
+    (34, 'Procedure II'),
+    (35, 'Procedure III'),
+    (36, 'Sports'),
+    (37, 'Immigration'),
+    (40, 'Votes'),
+    (41, 'Procedure IV'),
+    (42, 'Agriculture'),
+    (43, 'Hunger'),
+    (45, 'Boy Scouts')
+]
 
 def get_speaker_corpus_bow(speaker, speech_dict):
     '''Connect to mongodb and get the corpus of text'''
@@ -17,3 +55,15 @@ def get_speaker_corpus_bow(speaker, speech_dict):
 def get_speaker_topics(model, speaker, speech_dict):
     bow = get_speaker_corpus_bow(speaker, speech_dict)
     return model.get_document_topics(bow)
+
+def get_topic_name(index):
+    for (n,t) in TOPIC_LABELS:
+        if n == index:
+            return t
+    return 'Topic_'+str(index)
+
+def make_topic_plot(labels, vals):
+    source = ColumnDataSource(data=dict(topics=labels, focus=vals))
+    p = figure(y_range=labels)
+    p.hbar(y='topics', right='focus', source=source, height=0.8)
+    return p
--- hillspeech/hillspeech.py
+++ hillspeech/hillspeech.py
@@ -3,9 +3,10 @@ from flask_bootstrap import Bootstrap
 from gensim.models import LdaModel
 from gensim.corpora import Dictionary
 from pymongo import MongoClient
+from bokeh.embed import components
 import pickle
 
-from helpers import get_speaker_topics
+from helpers import get_speaker_topics, get_topic_name, make_topic_plot
 
 DICTIONARY_PATH = 'models/dictionary_97-07_and_10-18.dict'
 with open(DICTIONARY_PATH, 'rb') as f:
@@ -25,21 +26,18 @@ def index():
 
 @app.route('/speaker/<speaker>')
 def speaker(speaker):
-    topics = get_speaker_topics(model, speaker, dictionary) # Not currently using
 
-    client = MongoClient()
-    db = client['congressional-record']
-    speeches = db['speeches']   
-    search = {"speaker": speaker}
-    result = speeches.find_one(search)
-    client.close()
+    topics = get_speaker_topics(model,speaker, dictionary)
+    topics = [(get_topic_name(x[0]), x[1]) for x in topics]
 
-    if result:
-        content = result['text']
-    else:
-        content = 'No records returned'
+    topics.sort(key=lambda x:x[1])
+    labels = [x[0] for x in topics]
+    vals = [x[1] for x in topics]
 
-    return render_template('speaker.html', speaker=speaker, content=content)
+    p = make_topic_plot(labels, vals)
+    script, div = components(p)
+
+    return render_template('speaker.html', speaker=speaker, script=script, div=div)
 
 
 if __name__ == '__main__':
